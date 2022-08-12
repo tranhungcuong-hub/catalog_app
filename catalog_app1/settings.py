e87e7d0069819b86b834d355bbca9d45c4b91014
tranhungcuong-hub <80353022+tranhungcuong-hub@users.noreply.github.com>
2022-08-12 15:30:36 +0700

    sdasd

--- catalog_app1/settings.py
+++ catalog_app1/settings.py
@@ -190,6 +190,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
