1cdd043b0a0ddd135735ed0651c70feeb1858165
tranhungcuong-hub <80353022+tranhungcuong-hub@users.noreply.github.com>
2022-08-12 17:01:31 +0700

    sdasd

--- catalog_app1/settings.py
+++ catalog_app1/settings.py
@@ -191,11 +191,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(STATIC_ROOT, 'static'),
-)
+STATIC_URL = '/'
+print(BASE_DIR)
+STATIC_ROOT = os.path.join('/staticfiles/')
+# STATICFILES_DIRS = (
+#     os.path.join(STATIC_ROOT, 'staticfiles'),
+# )
 
 MEDIA_URL = '/media/'
 
