9b201f3b2fef5ff68e2bc2bf9487d469a6188355
tranhungcuong-hub <80353022+tranhungcuong-hub@users.noreply.github.com>
2022-08-12 15:56:02 +0700

    asdasdasd

--- catalog_app1/settings.py
+++ catalog_app1/settings.py
@@ -91,6 +91,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -190,7 +191,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
